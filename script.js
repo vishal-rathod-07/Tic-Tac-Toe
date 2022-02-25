a939d720963552abb8770ca461df790ca3c9a9a8
Vishal Rathod <rvd31305@gmail.com>
2022-02-25 11:53:39 +0530

    refactor: board, squares, checkForWinner, for loops
    - board: static board added instead of dynamic
    - squares: array of squares elements
    - checkForWinner: logic changed
    - for loops: replaced with for each loops

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const message = document.querySelector(".message");
 const gameover = document.querySelector(".game-over");
 const resetBtn = document.querySelector("#reset");
 
-message.innerHTML = "Player One's turn: 𝕏";
-message.style.color = "rgb(235, 106, 106)";
-
 let turns = 0;
 let gameOver = false;
 let currentBoard = [];
+
+const red = "rgb(235, 106, 106)";
+const blue = "rgb(84, 146, 228)";
 
 // arrays of winning combinations
 const winningCombinations = [
@@ -22,88 +22,78 @@ const winningCombinations = [
   [2, 4, 6],
 ];
 
-// Creating the board
-for (i = 0; i < 9; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  square.dataset.squareId = i;
-  board.appendChild(square);
-}
+const squares = document.querySelectorAll(".square");
 
-const squares = Array.from(document.querySelectorAll(".square"));
-
-document.addEventListener("click", (e) => {
-  if (e.target.matches(".square") && !gameOver) {
-    if (e.target.classList.contains("player-one")) return;
-    if (e.target.classList.contains("player-two")) return;
+// add event listener to squares
+squares.forEach((square) => {
+  square.addEventListener("click", (e) => {
+    if (gameOver) {
+      return;
+    }
+    if (e.target.innerHTML !== "") return;
     if (turns % 2 === 0) {
       message.innerHTML = "Player Two's turn: 𝕆";
       message.style.color = "rgb(84, 146, 228)";
       e.target.classList.add("player-one");
-      e.target.innerText = "X";
+      e.target.innerHTML = "X";
       currentBoard[e.target.dataset.squareId] = "X";
-      turns++;
-      console.log("Current Turn: " + turns);
-      console.log("Current Board: " + currentBoard);
-      checkForWinner();
     } else {
       message.innerHTML = "Player One's turn: 𝕏";
-      message.style.color = "rgb(235, 106, 106)";
+      message.style.color = red;
       e.target.classList.add("player-two");
-      e.target.innerText = "O";
+      e.target.innerHTML = "O";
       currentBoard[e.target.dataset.squareId] = "O";
-      // console.log(currentBoard);
-      turns++;
-      console.log("Current Turn: " + turns);
-      console.log("Current Board: " + currentBoard);
-      checkForWinner();
     }
-  }
+    turns++;
+    checkForWinner();
+  });
 });
 
 //check winner from current board
 function checkForWinner() {
-  for (let i = 0; i < winningCombinations.length; i++) {
-    if (
-      currentBoard[winningCombinations[i][0]] === "X" &&
-      currentBoard[winningCombinations[i][1]] === "X" &&
-      currentBoard[winningCombinations[i][2]] === "X"
-    ) {
-      message.innerHTML = "Player One wins 👑";
-      message.style.color = "rgb(235, 106, 106)";
-      gameOver = true;
-    } else if (
-      currentBoard[winningCombinations[i][0]] === "O" &&
-      currentBoard[winningCombinations[i][1]] === "O" &&
-      currentBoard[winningCombinations[i][2]] === "O"
-    ) {
-      message.innerHTML = "Player Two wins 👑";
-      message.style.color = "rgb(84, 146, 228)";
-      gameOver = true;
-    }
-  }
-  if (turns === 9 && gameOver === false) {
+  if (turns === 9 && !gameOver) {
     message.innerHTML = "It's a draw 🤷‍♀️";
     message.style.color = "rgb(136, 108, 228)";
     gameOver = true;
   }
-  if (gameOver === true) {
-    gameover.style.display = "block";
-  }
+
+  winningCombinations.forEach((combination) => {
+    const squareOne = currentBoard[combination[0]];
+    const squareTwo = currentBoard[combination[1]];
+    const squareThree = currentBoard[combination[2]];
+
+    if (squareOne === squareTwo && squareTwo === squareThree) {
+      if (squareOne === "X") {
+        message.innerHTML = "Player One wins 👑";
+        message.style.color = red;
+        gameOver = true;
+      } else if (squareOne === "O") {
+        message.innerHTML = "Player Two wins 👑";
+        message.style.color = "rgb(84, 146, 228)";
+        gameOver = true;
+      }
+    }
+
+    if (gameOver) {
+      gameover.style.display = "block";
+      return;
+    }
+  });
 }
 
 function reset() {
-  console.clear();
-  for (let i = 0; i < squares.length; i++) {
-    squares[i].classList.remove("player-one");
-    squares[i].classList.remove("player-two");
-    squares[i].innerText = "";
-  }
   turns = 0;
   gameOver = false;
-  gameover.style.display = "none";
   currentBoard = [];
   message.innerHTML = "Player One's turn: 𝕏";
+  message.style.color = red;
+  gameover.style.display = "none";
+
+  squares.forEach((square) => {
+    square.innerHTML = "";
+    square.classList.remove("player-one");
+    square.classList.remove("player-two");
+  });
 }
 
 resetBtn.addEventListener("click", reset);
